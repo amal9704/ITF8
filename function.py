494271704ebe0dfca88d713e270e84db68f61b4d
amal <1979amal2004.com>
2023-08-02 16:54:55 +0300

    fifth Commit

--- function.py
+++ function.py
@@ -1,28 +1,18 @@
-def add():
-    num1 = input("enter num1")
-    num2 = input('enter num2')
-    result = int(num1) + int(num2)
-    print(result)
-    return result
 
-def subtraction():
-    num1 = input("enter num1")
-    num2 = input("enter num2")
-    result = int(num1) - int(num2)
-    print(result)
-    return result
+num1 = int(input("enter num1"))
+num2 = int(input('enter num2'))
 
-def multuipulation():
-    num1 = input("enter num1")
-    num2 = input("enter num2")
-    result = int(num1) * int(num2)
-    print(result)
-    return result
-def division():
-    num1 = input("enter num1")
-    num2 = input("enter num2")
-    result = int(num1) / int(num2)
-    print(result)
-    return result
+def add(num1,num2):
+    return num1 + num2
+def subtraction(num1,num2):
+    return num1 - num2
 
+def multuipulation(num1,num2):
+    return num1 * num2
 
+def division(num1,num2):
+    return num1 / num2
+print(add(num1,num2))
+print(subtraction(num1,num2))
+print(multuipulation(num1,num2))
+print(division(num1,num2))
